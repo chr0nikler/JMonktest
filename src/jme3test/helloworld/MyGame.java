3980a1f9d45faf32950a6a84c7a4570e5ae02322
abcde13 <machspeeds@gmail.com>
2012-12-21 11:08:49 -0800

    Updated to walk properly. Jump animation not using

--- src/jme3test/helloworld/MyGame.java
+++ src/jme3test/helloworld/MyGame.java
@@ -10,14 +10,12 @@ import com.jme3.animation.AnimEventListener;
 import com.jme3.animation.LoopMode;
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.InputListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
@@ -26,7 +24,6 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 /**
  *
@@ -46,6 +43,7 @@ public class MyGame extends SimpleApplication implements ActionListener, AnimEve
     private boolean left = false, right = false;
     private CharacterControl player;
     Node playerModel;
+    private float airtime = 0;
     
     @Override
     public void simpleInitApp() {
@@ -58,15 +56,17 @@ public class MyGame extends SimpleApplication implements ActionListener, AnimEve
         stateManager.attach(bulletAppState);
         
         //First, create geometry (or in this case, the model)
-        playerModel = (Node) assetManager.loadModel("Models/ogre/Circle.mesh.j3o");
+        playerModel = (Node) assetManager.loadModel("Models/Oto/Oto.mesh.j3o");
         playerModel.setLocalScale(0.5f);
-        playerModel.setLocalTranslation(0,-1f,0f);
+        playerModel.setLocalTranslation(0,0f,0f);
         Node center = new Node();
         center.attachChild(playerModel);
         
         control = playerModel.getControl(AnimControl.class);
         control.addListener(this);
         channel = control.createChannel();
+        channel.setLoopMode(LoopMode.DontLoop);
+        channel.setSpeed(4f);
         //channel.setAnim("my_animation"); 
         //Next, attach material (no material needed for model)
         /*Material p1mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
@@ -74,7 +74,7 @@ public class MyGame extends SimpleApplication implements ActionListener, AnimEve
         p1.setMaterial(p1mat);*/
         
         //Then, create physics
-        CapsuleCollisionShape character = new CapsuleCollisionShape(1.2f,1.1f,1);
+        CapsuleCollisionShape character = new CapsuleCollisionShape(2.2f,2.2f,1);
         player = new CharacterControl(character,0.1f);
         player.setGravity(15f);
         center.addControl(player);
@@ -112,6 +112,8 @@ public class MyGame extends SimpleApplication implements ActionListener, AnimEve
         chasecam.setDefaultVerticalRotation(FastMath.PI/1.1f);
         
         initKeys();
+        
+        for (String anim : control.getAnimationNames()) { System.out.println(anim); }
     }
     
     private void initKeys() {
@@ -130,8 +132,7 @@ public class MyGame extends SimpleApplication implements ActionListener, AnimEve
                 left = true;
             } else {
                 left = false;
-            }
-            
+            }            
         }
         else if (binding.equals("Right")) {
             if(value) {
@@ -142,12 +143,14 @@ public class MyGame extends SimpleApplication implements ActionListener, AnimEve
         } 
         else if (binding.equals("Jump")){
             player.jump();
-            channel.setAnim("my_animation");
         }
+        
     }
     
     @Override
     public void simpleUpdate(float tpf) {
+        
+        
         Vector3f camLeft = cam.getLeft().clone().multLocal(0.25f);
         walkDirection.set(0,0,0);
         if(left){
@@ -156,13 +159,33 @@ public class MyGame extends SimpleApplication implements ActionListener, AnimEve
         if(right){
             walkDirection.addLocal(camLeft.negate());
         } 
+       
+        if(!player.onGround()){
+            airtime += tpf;
+        } else {
+            airtime = 0;
+        }
+        
+        if(walkDirection.length() == 0){
+            if(!"stand".equals(channel.getAnimationName())){
+                channel.setAnim("stand", 0.5f);
+            }
+        } else {
+            player.setViewDirection(walkDirection);
+            if(airtime > 0.3f){
+                if (!"stand".equals(channel.getAnimationName())) {
+                    channel.setAnim("stand");
+                }
+            } else if (!"Walk".equals(channel.getAnimationName())) {
+                  channel.setAnim("Walk", 0.7f);
+            }
+        }
         player.setWalkDirection(walkDirection);
     }
 
     public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
-       
     }
-
+    
     public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
        // throw new UnsupportedOperationException("Not supported yet.");
     }
